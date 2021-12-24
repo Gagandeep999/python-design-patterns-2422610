28b325c0110dcd961758a1a0a855e24582075f79
GAGANDEEP SINGH <gagan.rishi999@gmail.com>
2021-12-24 11:01:42 -0500

    Updated factory.py class

--- factory.py
+++ factory.py
@@ -7,3 +7,22 @@ class Dog:
 
 	def speak(self):
 		return "Woof!"
+
+class Cat:
+
+	"""A simple cat class"""
+
+	def __init__(self, name):
+		self._name = name
+
+	def speak(self):
+		return "Meow!"
+
+def get_pet(pet="dog"):
+	""" The Factory Method """
+
+	pets = dict(dog=Dog("Hope"), cat=Cat("Peace"))
+	return pets[pet]
+
+d = get_pet("cat")
+print(d.speak())
